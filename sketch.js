b11b9b67308f53409b95206161f74fbadfbe9e29
nandhinisantosh228 <nandhinisantosh228@gmail.com>
2020-11-16 15:15:48 +0530

    log

--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint=Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg;
 var platform;
+var constrainedLog;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -17,6 +19,7 @@ function setup(){
     world = engine.world;
 
     
+    constrainedLog=new Log(230,180,80,PI/2);
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150,305,300,170);
 
@@ -36,6 +39,16 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+
+    var options= {
+        bodyA: bird.body,
+        bodyB:constrainedLog.body,
+        stiffness:0.04,
+        length:10
+    }
+
+    var chain=constraint.create(options);
+    World.add(world,chain);
 
 }
 
@@ -62,4 +75,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    constrainedLog.display();
 }
